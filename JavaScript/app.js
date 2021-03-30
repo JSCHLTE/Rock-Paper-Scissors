a07c040f6aea5c42a612209f1cea2c4aed28f362
Jordan <phoaxy@gmail.com>
2021-03-30 16:43:46 -0400

    Commit

---
+++ JavaScript/app.js
@@ -0,0 +1,83 @@
+const rock = document.querySelector(".fa-hand-rock");
+const paper = document.querySelector(".fa-hand-paper");
+const scissors = document.querySelector(".fa-hand-scissors");
+const appendUserChoice = document.querySelector(".playerChose");
+const appendComputerChoice = document.querySelector(".computerChose");
+const appendWinner = document.querySelector(".winner");
+const appendUserWinCount = document.querySelector(".userWinCount");
+const appendComputerWinCount = document.querySelector(".computerWinCount");
+const gamesPlayed = document.querySelector(".gamesPlayed");
+
+let userWins = 0;
+let computerWins = 0;
+
+game = ()=>{
+
+    rock.addEventListener("click", ()=>{
+        let rng = Math.floor(Math.random()*3);
+        appendUserChoice.innerHTML = `You chose: rock`;
+        if(rng == 0){
+            appendComputerChoice.innerHTML = `Computer chose: rock`;
+            appendWinner.innerHTML = `The match is a tie.`
+        }
+        if(rng == 1){
+            appendComputerChoice.innerHTML = `Computer chose: paper`;
+            appendWinner.innerHTML = `Computer wins!`
+            computerWins++;
+        }
+        if(rng == 2){
+            appendComputerChoice.innerHTML = `Computer chose: scissors`;
+            appendWinner.innerHTML = `You win!`
+            userWins++;
+        }
+        appendUserWinCount.innerHTML = (userWins > 1 || userWins == 0) ? `${userWins} wins`: `${userWins} win`;
+        appendComputerWinCount.innerHTML = (computerWins > 1 || computerWins == 0) ? `${computerWins} wins`: `${computerWins} win`;
+        gamesPlayed.innerHTML++;
+    });
+
+    paper.addEventListener("click", ()=>{
+        let rng = Math.floor(Math.random()*3);
+        appendUserChoice.innerHTML = `You chose: paper`;
+        if(rng == 0){
+            appendComputerChoice.innerHTML = `Computer chose: rock`;
+            appendWinner.innerHTML = `You win!`
+            userWins++;
+        }
+        if(rng == 1){
+            appendComputerChoice.innerHTML = `Computer chose: paper`;
+            appendWinner.innerHTML = `The match is a tie.`
+        }
+        if(rng == 2){
+            appendComputerChoice.innerHTML = `Computer chose: scissors`;
+            appendWinner.innerHTML = `Computer wins!`
+            computerWins++;
+        }
+        appendUserWinCount.innerHTML = (userWins > 1 || userWins == 0) ? `${userWins} wins`: `${userWins} win`;
+        appendComputerWinCount.innerHTML = (computerWins > 1 || computerWins == 0) ? `${computerWins} wins`: `${computerWins} win`;
+        gamesPlayed.innerHTML++;
+    });
+
+    scissors.addEventListener("click", ()=>{
+        let rng = Math.floor(Math.random()*3);
+        appendUserChoice.innerHTML = `You chose: scissors`;
+        if(rng == 0){
+            appendComputerChoice.innerHTML = `Computer chose: rock`;
+            appendWinner.innerHTML = `Computer wins!`
+            computerWins++;
+        }
+        if(rng == 1){
+            appendComputerChoice.innerHTML = `Computer chose: paper`;
+            appendWinner.innerHTML = `You win!`
+            userWins++;
+        }
+        if(rng == 2){
+            appendComputerChoice.innerHTML = `Computer chose: scissors`;
+            appendWinner.innerHTML = `The match is a tie.`
+        }
+        appendUserWinCount.innerHTML = (userWins > 1 || userWins == 0) ? `${userWins} wins`: `${userWins} win`;
+        appendComputerWinCount.innerHTML = (computerWins > 1 || computerWins == 0) ? `${computerWins} wins`: `${computerWins} win`;
+        gamesPlayed.innerHTML++;
+    });
+};
+
+game();
